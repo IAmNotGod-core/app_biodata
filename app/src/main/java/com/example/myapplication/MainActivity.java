693bc6bd3d231c6d99ace5847ccfa8b98f2b128b
IAmNotGod <lintangkk09@gmail.com>
2022-10-17 10:16:46 +0700

    Add quote and buttons

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAddr = findViewById(R.id.btnAddr);
+        Button btnTelp = findViewById(R.id.btnTelp);
+        Button btnEmail = findViewById(R.id.btnEmail);
+
+        btnAddr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Jl. Magersari II No.56 Pendrikan Kidul", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnTelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "082242161769", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "lintngkk09@gmail.com", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
